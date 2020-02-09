6e8c3d8ea83684377c80884180d20f660a59609d
Bogdan Beldea <beldeabogdan@gmail.com>
2020-02-09 21:40:09 +0200

    Initial commit.

---
+++ src/main/java/com/beldeabogdan/binovate/BinovateApplication.java
@@ -0,0 +1,13 @@
+package com.beldeabogdan.binovate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinovateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinovateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beldeabogdan/binovate/BinovateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beldeabogdan.binovate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinovateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
